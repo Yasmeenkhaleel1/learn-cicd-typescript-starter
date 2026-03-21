2007551dceca53f6cb3dadca5b74c25ef5317fd6
Yasmeenkhaleel1 <yasmeenkhaleel2003@gmail.com>
2026-03-22 00:46:44 +0200

    fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,15 +4,11 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  // ❌ تجاهل ملفات البناء
   {
     ignores: ["dist/**"],
   },
-
-  // ✅ إعدادات TypeScript + JS
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
+    files: ["src/**/*.{ts,js}"],
     languageOptions: {
       globals: globals.node,
     },
